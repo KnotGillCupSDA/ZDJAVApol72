6486e105150a4239920a8fc21e9179bff4ad7ca1
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-01-24 15:57:48 +0100

    -- PersonDetails: isChild() test

--- src/main/java/com/sda/testingbasics/personal/PersonDetails.java
+++ src/main/java/com/sda/testingbasics/personal/PersonDetails.java
@@ -2,4 +2,17 @@ package com.sda.testingbasics.personal;
 
 public class PersonDetails {
 
+    private final String name;
+    private final String surname;
+    private final int age;
+
+    public PersonDetails(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean isChild() {
+        return age >= 0 && age <= 10;
+    }
 }
--- src/test/java/com/sda/testingbasics/personal/PersonDetailsTest.java
+++ src/test/java/com/sda/testingbasics/personal/PersonDetailsTest.java
@@ -1,13 +1,13 @@
 package com.sda.testingbasics.personal;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PersonDetailsTest {
 
     @Test
     void thatPersonIsAChild() {
-
+        PersonDetails personDetails = new PersonDetails("Tomek", "Wozniak", 5);
+        Assertions.assertTrue(personDetails.isChild());
     }
 }
