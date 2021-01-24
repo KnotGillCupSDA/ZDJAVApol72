c72f881d2d57855111397c0b6cd2c8429481d064
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-01-24 11:28:52 +0100

    -- LifecycleMethodsExampleTest done

--- src/test/java/com/sda/testingbasics/LifecycleMethodsExampleTest.java
+++ src/test/java/com/sda/testingbasics/LifecycleMethodsExampleTest.java
@@ -1,13 +1,27 @@
 package com.sda.testingbasics;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class LifecycleMethodsExampleTest {
 
     @BeforeEach
     void setUp() {
         System.out.println("Method called before each test-method");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Method called after each test-method");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Method called once before all test-methods");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Method called once after all test-methods");
     }
 
     @Test
@@ -18,5 +32,10 @@ public class LifecycleMethodsExampleTest {
     @Test
     void mySecondTestMethod() {
         System.out.println("My second test method");
+    }
+
+    @Test
+    void myThirdTestMethod() {
+        System.out.println("My third test method");
     }
 }
