b8073408a746c0d6461b2ae3bb7a76e75a2a9a95
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-01-24 15:39:25 +0100

    -- PersonDetails: scratch

---
+++ src/test/java/com/sda/testingbasics/personal/PersonDetailsTest.java
@@ -0,0 +1,13 @@
+package com.sda.testingbasics.personal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDetailsTest {
+
+    @Test
+    void thatPersonIsAChild() {
+
+    }
+}
