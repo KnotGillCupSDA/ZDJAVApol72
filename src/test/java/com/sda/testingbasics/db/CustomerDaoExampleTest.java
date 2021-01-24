28c4647dcd6f49c576c93523c55f484de3802c3c
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-01-24 14:58:18 +0100

    -- AssertJ: first CustomerDao test

---
+++ src/test/java/com/sda/testingbasics/db/CustomerDaoExampleTest.java
@@ -0,0 +1,46 @@
+package com.sda.testingbasics.db;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+class CustomerDaoExampleTest {
+
+    @Test
+    void thatWeCanAddCustomer() {
+        CustomerDao dao = new CustomerDaoImpl();
+        String expectedName = "Tomek";
+        Customer tomek = new Customer(expectedName, "wozdev@gmail.com");
+
+        dao.add(tomek);
+
+        Collection<Customer> all = dao.getAll();
+
+        /*Assertions.assertNotNull(all);
+        Assertions.assertFalse(all.isEmpty());
+        Assertions.assertEquals(1, all.size());
+
+        Customer retrievedCustomer = all.iterator().next();
+        Assertions.assertNotNull(retrievedCustomer);
+
+        String name = retrievedCustomer.getName();
+        Assertions.assertEquals(expectedName, name);*/
+
+        Assertions.assertThat(all)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(1)
+                .containsOnly(tomek);
+
+        /* without using .equals()
+        Assertions.assertThat(all)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(1)
+                .element(0)
+                .extracting(Customer::getName)
+                .isEqualTo(expectedName);*/
+
+    }
+}
