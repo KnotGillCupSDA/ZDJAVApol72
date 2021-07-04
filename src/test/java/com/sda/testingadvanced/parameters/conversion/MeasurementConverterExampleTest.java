bf3ee1bf3bc3b756eeab61e12cc0f0ded1121a81
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-07-04 13:54:04 +0200

    -- testing advanced MeasurementConverterExampleTest unused import

--- src/test/java/com/sda/testingadvanced/parameters/conversion/MeasurementConverterExampleTest.java
+++ src/test/java/com/sda/testingadvanced/parameters/conversion/MeasurementConverterExampleTest.java
@@ -9,7 +9,6 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
