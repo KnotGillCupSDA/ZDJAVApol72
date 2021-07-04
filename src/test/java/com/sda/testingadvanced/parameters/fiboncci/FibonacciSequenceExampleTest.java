1c3612b75514bc5a83d65f5be1b3fb337f406f99
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-07-04 14:06:20 +0200

    -- testing advanced FibonacciSequenceExampleTest assertThrows

--- src/test/java/com/sda/testingadvanced/parameters/fiboncci/FibonacciSequenceExampleTest.java
+++ src/test/java/com/sda/testingadvanced/parameters/fiboncci/FibonacciSequenceExampleTest.java
@@ -1,10 +1,13 @@
 package com.sda.testingadvanced.parameters.fiboncci;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigInteger;
 import java.util.stream.Stream;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -19,6 +22,17 @@ class FibonacciSequenceExampleTest {
 
 		assertEquals(new BigInteger("701408733"),
 				fibonacciSequence.getFibonacciNumber(44));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideImplementations")
+	void shouldThrowsExceptionForNegativeNumbers(FibonacciSequence fibonacciSequence) {
+		// JUnit5
+		assertThrows(IllegalArgumentException.class, () -> fibonacciSequence.getFibonacciNumber(-4));
+
+		//AssertJ
+		assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> fibonacciSequence.getFibonacciNumber(-4));
 	}
 
 	private static Stream<FibonacciSequence> provideImplementations() {
