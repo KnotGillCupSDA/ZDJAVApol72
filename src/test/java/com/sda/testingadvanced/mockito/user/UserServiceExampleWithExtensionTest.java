12678b1db1742b5326774c86a8683de82023af48
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-07-04 14:48:47 +0200

    -- testing advanced UserServiceExampleWithExtensionTest first test case

--- src/test/java/com/sda/testingadvanced/mockito/user/UserServiceExampleWithExtensionTest.java
+++ src/test/java/com/sda/testingadvanced/mockito/user/UserServiceExampleWithExtensionTest.java
@@ -1,6 +1,9 @@
 package com.sda.testingadvanced.mockito.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
@@ -24,20 +27,34 @@ class UserServiceExampleWithExtensionTest {
 	private UserService userService;
 
 	@Test
-	void shouldFindUser() {
-		Mockito
-				.when(userRepository.findById(4L))
+	void experiments() {
+		when(userRepository.findById(4L))
 				.thenReturn(
 						Optional.of(new User(4L, "Tomasz", "Wozniak")));
 
-		Mockito.lenient().when(userRepository.findById(5L)).thenReturn(Optional.empty());
+		lenient().when(userRepository.findById(5L)).thenReturn(Optional.empty());
 
 		final User userById = userService.getUserById(4L);
 
 		System.out.println(userById);
 		assertNotNull(userById);
 
-		Mockito.verify(userRepository, Mockito.times(1)).findById(4L);
-		Mockito.verifyNoInteractions(userValidator);
+		verify(userRepository, times(1)).findById(4L);
+		verifyNoInteractions(userValidator);
+	}
+
+	@Test
+	void shouldFindUserById() {
+		//given
+		final Long userId = 1L;
+		final User user = new User(userId, "MockedName", "MockedLastName");
+		when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+		//when
+		final Optional<User> byId = userRepository.findById(userId);
+
+		//then
+		assertFalse(byId.isEmpty());
+		assertEquals(user, byId.get());
 	}
 }
