3db296f0a669a8ed2d9124c1c0d12f11245fe22e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-07-04 10:18:20 +0200

    -- testing advanced DivisibilityChecker

---
+++ src/main/java/com/sda/testingadvanced/parameters/DivisibilityChecker.java
@@ -0,0 +1,9 @@
+package com.sda.testingadvanced.parameters;
+
+public class DivisibilityChecker {
+
+	public boolean isDivisibleBy3() {
+		return false;
+	}
+
+}
---
+++ src/test/java/com/sda/testingadvanced/parameters/DivisibilityCheckerTest.java
@@ -0,0 +1,15 @@
+package com.sda.testingadvanced.parameters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DivisibilityCheckerTest {
+
+	@ParameterizedTest
+	@ValueSource
+	void shouldBeDivisibleBy3() {
+
+	}
+}
