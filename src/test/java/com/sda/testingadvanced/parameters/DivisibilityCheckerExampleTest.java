a8a960da8d67cdf664e1160f773745c63fa41625
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-07-04 10:18:56 +0200

    -- testing advanced DivisibilityCheckerExampleTest

--- src/test/java/com/sda/testingadvanced/parameters/DivisibilityCheckerExampleTest.java
+++ src/test/java/com/sda/testingadvanced/parameters/DivisibilityCheckerExampleTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class DivisibilityCheckerTest {
+class DivisibilityCheckerExampleTest {
 
 	@ParameterizedTest
 	@ValueSource
